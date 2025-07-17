077b289629e2d6fcdacd9e35658787e4b5d63d80
Malte Ubl <malte.ubl@gmail.com>
2025-07-17 13:52:38 -0700

    Reduce idle timeout

--- lib/db.ts
+++ lib/db.ts
@@ -17,7 +17,7 @@ const pool = new Pool({
   ssl: sslConfig,
   // Connection pool configuration
   max: 50, // Maximum number of clients in the pool
-  idleTimeoutMillis: 30000, // How long a client is allowed to remain idle before being closed
+  idleTimeoutMillis: 5000, // How long a client is allowed to remain idle before being closed
   connectionTimeoutMillis: 2000, // How long to wait for a connection
 });
 
