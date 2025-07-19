49d207b5be9c1541c89dda45a2617f64ab6fede8
Malte Ubl <malte.ubl@gmail.com>
2025-07-19 12:13:34 -0700

    redis-stats

--- app/api/benchmark/route.ts
+++ app/api/benchmark/route.ts
@@ -1,6 +1,10 @@
 import { getPoolMetrics, query, transaction } from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 import { nanoid } from "nanoid";
+import { createClient } from "redis";
+
+const redis = createClient();
+const connectPromise = redis.connect();
 
 function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
@@ -8,14 +12,21 @@ function sleep(ms: number) {
 
 const instanceId = nanoid(6);
 let concurrency = 0;
+const deployment = process.env.VERCEL_URL || "local";
+
+function getRedisKey() {
+  return `pooling-benchmark:${deployment}`;
+}
 
 export async function GET(request: NextRequest) {
+  await connectPromise;
   const sleepMs = request.nextUrl.searchParams.get("sleepMs");
   const runInTransaction = Boolean(
     request.nextUrl.searchParams.get("runInTransaction") || false
   );
+  const redisConcurrency = parseInt((await redis.get(getRedisKey())) || "0");
   const metrics = getPoolMetrics();
-  console.log("before metrics", metrics);
+  console.log("before metrics", metrics, redisConcurrency);
   const before = Date.now();
   const startConcurrency = concurrency + 1;
   const success = runInTransaction
@@ -25,14 +36,16 @@ export async function GET(request: NextRequest) {
           async (text, params) => {
             const result = await client.query(text, params);
             return result.rows;
-          }
+          },
+          redis
         );
       })
-    : await runQuery(parseInt(sleepMs || "0"), query);
+    : await runQuery(parseInt(sleepMs || "0"), query, redis);
   const queryEnd = Date.now();
 
   const data = {
     success,
+    redisConcurrency: redisConcurrency + 1,
     inFunctionConcurrency: startConcurrency,
     instanceId,
     poolMetrics: metrics,
@@ -48,10 +61,15 @@ export async function GET(request: NextRequest) {
   return NextResponse.json(data);
 }
 
-async function runQuery(sleepMs: number, runQuery: typeof query) {
+async function runQuery(
+  sleepMs: number,
+  runQuery: typeof query,
+  redis: ReturnType<typeof createClient>
+) {
   try {
     // Execute a simple query to fetch one user
     concurrency++;
+    await redis.incr(getRedisKey());
     const users = await runQuery(`
       SELECT id
       FROM users
@@ -67,5 +85,6 @@ async function runQuery(sleepMs: number, runQuery: typeof query) {
     return false;
   } finally {
     concurrency--;
+    await redis.decr(getRedisKey());
   }
 }
